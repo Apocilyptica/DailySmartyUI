554d85540cf3f97d4fcc484e38a9dc7328a62648
James Jager <jviper882000@gmail.com>
2020-07-21 03:41:19 -0400

    Created Recent post and search bar comments and used them in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
+
 import Logo from "./logo";
+import SearchBar from "./searchbar";
+import RecentPosts from "./recentposts";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div>
-          <Logo />
-        </div>
-        <div>
-          <input placeholder="Search Bar" />
-        </div>
-        <div>
-          Links here
-        </div>
+        <Logo />
+        <SearchBar />
+        <RecentPosts />
       </div>
     );
   }
