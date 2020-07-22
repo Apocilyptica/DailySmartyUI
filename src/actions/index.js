176ac8eb880ad3c2d12d81e21a3c7bc761635c4c
James Jager <jviper882000@gmail.com>
2020-07-22 06:22:36 -0400

    added post topics to post component

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@ import actions from "redux-form/lib/actions";
 export function fetchRecentPosts() {
   return function (dispatch) {
     axios.get("http://api.dailysmarty.com/posts").then((response) => {
+      console.log("response", response);
       dispatch({
         type: SET_RECENT_POSTS,
         payload: response.data.posts,
